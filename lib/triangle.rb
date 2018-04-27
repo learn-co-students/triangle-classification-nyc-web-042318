bc9b8cefe290e455d708a26c61f9030d4ef8adce
github username <jeffreyhwang90@gmail.com>
2018-04-27 17:23:44 -0400

    try angles

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  ALL = []
+  attr_accessor :l_1, :l_2, :l_3
+
+  def initialize(l_1, l_2, l_3)
+    @l_1 = l_1
+    @l_2 = l_2
+    @l_3 = l_3
+
+    ALL << self
+  end
+
+  def kind
+    if @l_1 < 0 || @l_2 < 0 || @l_3 < 0 || (@l_1 >= (@l_2 + @l_3)) || (@l_2 >= (@l_1 + @l_3)) || (@l_3 >= (@l_1 + @l_2))
+      begin
+        raise TriangleError
+          puts error.message
+      end
+    end
+    if @l_1 == @l_2 && @l_2 == @l_3
+      return :equilateral
+    elsif @l_1 == @l_2 || @l_2 == @l_3 || @l_3 == @l_1
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Duuuude, I want a triangle!"
+    end
+  end
+
 end
